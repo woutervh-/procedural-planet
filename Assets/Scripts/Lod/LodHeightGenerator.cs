17f10b5ea171f00a18a664159e8142dd60184aaa
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2019-11-18 14:26:01 +0100

    Refactor to make better use of noise computation

--- Assets/Scripts/Lod/LodHeightGenerator.cs
+++ Assets/Scripts/Lod/LodHeightGenerator.cs
@@ -9,32 +9,13 @@ public class LodHeightGenerator
         this.perlin = perlin;
     }
 
-    public float GetHeight(Vector3 position)
+    public Perlin.PerlinSample GetSample(Vector3 position)
     {
-        return (this.perlin.Sample(position.x, position.y, position.z) / 2f + 1.5f).value;
+        return this.perlin.Sample(position.x, position.y, position.z) / 2f;
     }
 
-    public Vector3 GetHeightGradient(Vector3 position)
+    public static Vector3 GetAdjustedNormal(Vector3 normal, Vector3 derivative)
     {
-        return (this.perlin.Sample(position.x, position.y, position.z) / 2f).derivative;
-    }
-
-    public Vector3 GetNormal(Vector3 normal, Vector3 position)
-    {
-        // // Central difference method.
-        // float h = 2f;
-        // float f = this.perlin.Sample(position.x, position.y, position.z).value;
-        // float fx0 = this.perlin.Sample(position.x - h, position.y, position.z).value;
-        // float fy0 = this.perlin.Sample(position.x, position.y - h, position.z).value;
-        // float fz0 = this.perlin.Sample(position.x, position.y, position.z - h).value;
-        // float fx1 = this.perlin.Sample(position.x + h, position.y, position.z).value;
-        // float fy1 = this.perlin.Sample(position.x, position.y + h, position.z).value;
-        // float fz1 = this.perlin.Sample(position.x, position.y, position.z + h).value;
-        // Vector3 df = new Vector3((fx0 + fx1) / 2f / h, (fy0 + fy1) / 2f / h, (fz0 + fz1) / 2f / h);
-        // return (normal - df).normalized;
-
-        // Analytical method.
-        Vector3 df = this.GetHeightGradient(position);
-        return (normal - df).normalized;
+        return (normal - derivative).normalized;
     }
 }
